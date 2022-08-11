22e31181305e529ebad7b4ca795ef592105be1f8
Yeseul Kim <94942842+yk170901@users.noreply.github.com>
2022-08-11 12:33:30 +0900

    Random Replacement Done

--- QuickCrossword/Controller/CrosswordController.cs
+++ QuickCrossword/Controller/CrosswordController.cs
@@ -17,12 +17,12 @@ namespace QuickCrossword.Controller
         private static CrosswordController _instance = null;
         public static CrosswordController Instance()
         {
-            if(_instance == null)
+            if (_instance == null)
                 _instance = new CrosswordController();
             return _instance;
         }
 
-        // PUBLIC
+        // PUBLIC, IMPORTANT
         /// <summary>
         /// 
         /// </summary>
@@ -30,7 +30,11 @@ namespace QuickCrossword.Controller
         public char[]? GetBoard(BoardMode boardMode = BoardMode.TenXTen)
         {
             BoardSize = (int)boardMode;
-            CreateBoard();
+
+            WordAndClue[] WordAndClueArray = GetWACFromDb();
+
+            PutWordsOnBoard(WordAndClueArray);
+
             return _board;
         }
 
@@ -58,44 +62,216 @@ namespace QuickCrossword.Controller
             return WordAndClueList.ToArray();
         }
 
-        // IMPORTANT
-        /// <summary>
-        /// Create matrix of crossword based on data from Sqlite DB
-        /// </summary>
-        private void CreateBoard()
-        {
-            // Will contain tons of methods
-            WordAndClue[] WordAndClueArray = GetWACFromDb();
-
-            PutWordsInMatrix(WordAndClueArray);
-        }
-
         /// <summary>
         /// Check if the board is null as the length of the word
         /// </summary>
-        /// <param name="boardIdx"></param>
-        /// <param name="wordLength"></param>
-        /// <returns></returns>
-        private bool CheckIfNullAsWordLength(int boardIdx, int wordLength)
+        private bool CheckIfNullAsWordLengthHorizontally(int boardIdx, int wordLength)
         {
             // Horizontal
             for (int i = 1; i < wordLength; i++)
             {
-                // 배열의 오른쪽 끝단에 막히나 (BoardSize의 배수에 해당하나 확인)
-                // boardIdx + i + 1 = 배열은 0부터 시작하니 +1을 해줌
-                // -1 : 지금 확인하는 배열 index의 전의 index 숫자가 끝자락에 다다른 거면 지금 숫자는 그 범위를 넘어가서 문제인 거니 -1을 해줌) // board 그리드 x 범위 넘어서면
+                /* 배열의 오른쪽 끝단에 막히나 (BoardSize의 배수에 해당하나 확인)
+                   boardIdx + i + 1 = 배열은 0부터 시작하니 +1을 해줌
+                   -1 : 지금 확인하는 배열 index의 전의 index 숫자가 끝자락에 다다른 거면 지금 숫자는 그 범위를 넘어가서 문제인 거니 -1을 해줌) */
                 if ((boardIdx + i + 1 - 1) % BoardSize == 0
-                    || (boardIdx + i) >= BoardSize* BoardSize
+                    || (boardIdx + i) >= BoardSize * BoardSize
                     || !_board[boardIdx + i].Equals('\0')) return false;
             }
             return true;
-
         }
 
-        private bool CheckIfNearbyClearForRandomPlacement(int boardIdxInit, int wordLength)
+        // IS T?HIS RRIGHT??
+        /// <summary>
+        /// Check if the board is null as the length of the word
+        /// </summary>
+        private bool CheckIfNullAsWordLengthVertically(int boardIdx, int wordLength)
         {
             // Horizontal
-            int CellCheckNum = BoardSize / 3;
+            for (int i = boardIdx; i <= boardIdx+((wordLength - 1) * BoardSize); i = i + BoardSize)
+            {
+                if (i >= BoardSize * BoardSize
+                    || !_board[i].Equals('\0')) return false;
+            }
+            return true;
+        }
+
+        // Vertical
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="boardIdxInit"></param>
+        /// <param name="wordLength"></param>
+        /// <returns></returns>
+        private bool CheckIfDiagonalClearByOneVertically(int boardIdxInit, int wordLength)
+        {
+            int LastWordIdx = boardIdxInit + ((wordLength - 1) * BoardSize);
+
+            // Top Left
+            bool TopLeftClear = false;
+            int TopLeftIdx = (boardIdxInit - BoardSize) - 1;
+            if ((TopLeftIdx + 1) % BoardSize == 0 // 0부터 시작하는 배열이니 +1
+                || TopLeftIdx < 0
+                || _board[TopLeftIdx].Equals('\0')) TopLeftClear = true;
+
+            if (!TopLeftClear) return false;
+
+            // Bottom Left
+            bool BottomLeftClear = false;
+            int BottomLeftIdx = (LastWordIdx + BoardSize) - 1;
+            if ((BottomLeftIdx + 1) % BoardSize == 0 // 0부터 시작하는 배열이니 +1
+                || BottomLeftIdx >= BoardSize * BoardSize
+                || _board[BottomLeftIdx].Equals('\0')) BottomLeftClear = true;
+
+            if (!BottomLeftClear) return false;
+
+            // Top Right
+            bool TopRightClear = false;
+            int TopRightIdx = (boardIdxInit - BoardSize) + 1;
+            if ((TopRightIdx) % BoardSize == 0 // 왜 이건 그냥 해도 되지???
+                || TopRightIdx < 0
+                || _board[TopRightIdx].Equals('\0')) TopRightClear = true;
+
+            if (!TopRightClear) return false;
+
+            bool BottomRightClear = false;
+            // Bottom Right
+            int BottomRightIdx = (LastWordIdx + BoardSize) + 1; // 여기에서 이미 +1을 했기 때문에?
+            if ((BottomRightIdx) % BoardSize == 0 // 왜 이건 그냥 해도 되지???
+                || BottomRightIdx >= BoardSize * BoardSize
+                || _board[BottomRightIdx].Equals('\0')) BottomRightClear = true;
+
+            if (!BottomRightClear) return false;
+
+            return true;
+        }
+        private bool CheckIfDiagonalClearByOneHorizontally(int boardIdxInit, int wordLength)
+        {
+            int LastWordIdx = boardIdxInit + wordLength - 1;
+
+            // Top Left
+            bool TopLeftClear = false;
+            int TopLeftIdx = (boardIdxInit - BoardSize) - 1;
+            if ((TopLeftIdx + 1) % BoardSize == 0 // 0부터 시작하는 배열이니 +1
+                || TopLeftIdx < 0
+                || _board[TopLeftIdx].Equals('\0')) TopLeftClear = true;
+
+            if (!TopLeftClear) return false;
+
+            // Bottom Left
+            bool BottomLeftClear = false;
+            int BottomLeftIdx = (boardIdxInit + BoardSize) - 1;
+            if ((BottomLeftIdx + 1) % BoardSize == 0 // 0부터 시작하는 배열이니 +1
+                || BottomLeftIdx >= BoardSize * BoardSize
+                || _board[BottomLeftIdx].Equals('\0')) BottomLeftClear = true;
+
+            if (!BottomLeftClear) return false;
+
+            // Top Right
+            bool TopRightClear = false;
+            int TopRightIdx = (LastWordIdx - BoardSize) + 1;
+            if ((TopRightIdx) % BoardSize == 0 // 왜 이건 그냥 해도 되지???
+                || TopRightIdx < 0
+                || _board[TopRightIdx].Equals('\0')) TopRightClear = true;
+
+            if (!TopRightClear) return false;
+
+            bool BottomRightClear = false;
+            // Bottom Right
+            int BottomRightIdx = (LastWordIdx + BoardSize) + 1; // 여기에서 이미 +1을 했기 때문에?
+            if ((BottomRightIdx) % BoardSize == 0 // 왜 이건 그냥 해도 되지???
+                || BottomRightIdx >= BoardSize * BoardSize
+                || _board[BottomRightIdx].Equals('\0')) BottomRightClear = true;
+
+            if (!BottomRightClear) return false;
+
+            return true;
+        }
+
+        // 이거 다른 로직 덧붙한 검이
+        private bool CheckIfNearbyClearEnoughVertically(int boardIdxInit, int wordLength)
+        {
+            // Horizontal
+            int CellCheckNum = BoardSize / 2;
+
+            int LastWordIdx = boardIdxInit + ((wordLength -1) * BoardSize);
+
+            for (int CurrentBoardIdx = boardIdxInit; CurrentBoardIdx <= LastWordIdx; CurrentBoardIdx = CurrentBoardIdx + BoardSize) // CurrentBoardIdx += BoardSize
+            {
+                // currenBoardIdx = 18 25 32
+                for (int i = 1; i < CellCheckNum; i++)
+                {
+                    // 첫글자
+                    if (CurrentBoardIdx.Equals(boardIdxInit))
+                    {
+                        // 비교하려는 Index가 위 그리드 끝을 넘어서면 굳이 비교 x
+                        if (CurrentBoardIdx - (i * BoardSize) < 0)
+                        {
+                            goto SkipUpOrDown;
+                        }
+
+                        // Up
+                        if (!_board[CurrentBoardIdx - (i * BoardSize)].Equals('\0'))
+                        {
+                            return false;
+                        }
+                    }
+
+                    // 마지막 글자
+                    if (CurrentBoardIdx.Equals(LastWordIdx))
+                    {
+                        // 비교하려는 Index가 아래 그리드 끝을 넘어서면 굳이 비교 x
+                        if (CurrentBoardIdx + (i * BoardSize) >= (BoardSize * BoardSize))
+                        {
+                            goto SkipUpOrDown;
+                        }
+
+                        // Down
+                        if (!_board[CurrentBoardIdx + (i * BoardSize)].Equals('\0'))
+                        {
+                            return false;
+                        }
+                    }
+
+                SkipUpOrDown:;
+
+                    // 비교하려는 Index가 왼쪽 그리드 끝을 넘어서면 굳이 비교 x
+                    if (CurrentBoardIdx - i % BoardSize == 0
+                        || CurrentBoardIdx - i < 0)
+                    {
+                        goto SkipLeft;
+                    }
+
+                    // Left
+                    if (!_board[CurrentBoardIdx - i].Equals('\0'))
+                    {
+                        return false;
+                    }
+
+                SkipLeft:;
+
+                    // 비교하려는 Index가 오른쪽 그리드 끝을 넘어서면 굳이 비교 x
+                    if (CurrentBoardIdx + i % BoardSize == 0
+                        || CurrentBoardIdx + i >= BoardSize * BoardSize)
+                    {
+                        goto SkipRight;
+                    }
+
+                    // Right
+                    if (!_board[CurrentBoardIdx + i].Equals('\0'))
+                    {
+                        return false;
+                    }
+
+                SkipRight:;
+                }
+            }
+            return true;
+        }
+
+        private bool CheckIfNearbyClearEnoughHorizontally(int boardIdxInit, int wordLength)
+        {
+            // Horizontal
+            int CellCheckNum = BoardSize / 2;
 
             int LastWordIdx = boardIdxInit + wordLength - 1;
 
@@ -108,7 +284,10 @@ namespace QuickCrossword.Controller
                     {
                         // 비교하려는 Index가 왼쪽 그리드 끝을 넘어서면 굳이 비교 x
                         if (CurrentBoardIdx - i % BoardSize == 0
-                            || CurrentBoardIdx - i < 0) continue;
+                            || CurrentBoardIdx - i < 0)
+                        {
+                            goto SkipUpOrDown;
+                        }
 
                         // Left
                         if (!_board[CurrentBoardIdx - i].Equals('\0'))
@@ -122,7 +301,10 @@ namespace QuickCrossword.Controller
                     {
                         // 비교하려는 Index가 오른쪽 그리드 끝을 넘어서면 굳이 비교 x
                         if (CurrentBoardIdx + i % BoardSize == 0
-                            || CurrentBoardIdx + i >= BoardSize * BoardSize) continue;
+                            || CurrentBoardIdx + i >= BoardSize * BoardSize)
+                        {
+                            goto SkipUpOrDown;
+                        }
 
                         // Right
                         if (!_board[CurrentBoardIdx + i].Equals('\0'))
@@ -131,8 +313,13 @@ namespace QuickCrossword.Controller
                         }
                     }
 
+                SkipUpOrDown:;
+
                     // 비교하려는 Index가 위 그리드 끝을 넘어서면 굳이 비교 x
-                    if (CurrentBoardIdx - (i * BoardSize) < 0) continue;
+                    if (CurrentBoardIdx - (i * BoardSize) < 0)
+                    {
+                        goto SkipUp;
+                    }
 
                     // Up
                     if (!_board[CurrentBoardIdx - (i * BoardSize)].Equals('\0'))
@@ -140,15 +327,20 @@ namespace QuickCrossword.Controller
                         return false;
                     }
 
+                SkipUp:;
+
                     // 비교하려는 Index가 아래 그리드 끝을 넘어서면 굳이 비교 x
-                    if (CurrentBoardIdx + (i * BoardSize) >= (BoardSize * BoardSize)) continue;
+                    if (CurrentBoardIdx + (i * BoardSize) >= (BoardSize * BoardSize))
+                    {
+                        goto SkipDown;
+                    }
 
                     // Down
                     if (!_board[CurrentBoardIdx + (i * BoardSize)].Equals('\0'))
                     {
                         return false;
                     }
-
+                SkipDown:;
                 }
             }
 
@@ -160,7 +352,7 @@ namespace QuickCrossword.Controller
         /// <summary>
         /// Put random words from WordAndClue into the _board to see if they generates whole valid a board of 1 dimension with one another
         /// </summary>
-        private void PutWordsInMatrix(WordAndClue[] WordAndClueArray)
+        private void PutWordsOnBoard(WordAndClue[] WordAndClueArray)
         {
             _board = new char[BoardSize* BoardSize];
             Direction direction;
@@ -180,35 +372,18 @@ namespace QuickCrossword.Controller
                 bool CanPlace = false;
                 bool IsMatchingChar = false;
 
+
                 if (!Linked) // Put it at random location
                 {
-                    for (int boardIdx = 0; boardIdx < _board.Length; boardIdx++)
+                    if (cnt % 2 == 0)
                     {
-                        // 만약 해당 cell이 null이라면 
-                        if (_board[boardIdx].Equals('\0'))
-                        {
-                            bool nullAsLength = CheckIfNullAsWordLength(boardIdx, word.Length);
-                            if (!nullAsLength) continue;
-
-                            bool nearbyClearEnough = CheckIfNearbyClearForRandomPlacement(boardIdx, word.Length);
-                            if (!nearbyClearEnough) continue;
-
-                            bool diagonalClearByOne;
-
-                            for(int wordIdx = 0; wordIdx < word.Length; wordIdx++)
-                            {
-                                _board[boardIdx+ wordIdx] = word[wordIdx];
-                            }
-
-                            break;
-                        }
-
-
-
+                        PlaceHorizontallyRandom(word);
+                    }
+                    else
+                    {
+                        PlaceVerticallyRandom(word);
                     }
                 }
-
-
             }
 
             // grid 형태로 _board를 보기
@@ -224,6 +399,68 @@ namespace QuickCrossword.Controller
                     Debug.WriteLine("");
             }
 
+        }
+
+        // Vertical
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="word"></param>
+        private void PlaceVerticallyRandom(string? word)
+        {
+            for (int boardIdx = 0; boardIdx < _board.Length; boardIdx++)
+            {
+                if (_board[boardIdx].Equals('\0'))
+                {
+                    bool nullAsLength = CheckIfNullAsWordLengthVertically(boardIdx, word.Length);
+                    if (!nullAsLength) continue;
+
+                    bool nearbyClearEnough = CheckIfNearbyClearEnoughVertically(boardIdx, word.Length);
+                    if (!nearbyClearEnough) continue;
+
+                    bool diagonalClearByOne = CheckIfDiagonalClearByOneVertically(boardIdx, word.Length);
+                    if (!diagonalClearByOne) continue;
+
+                    for (int wordIdx = 0; wordIdx < word.Length; wordIdx++)
+                    {
+                        _board[boardIdx + (wordIdx * BoardSize)] = word[wordIdx];
+                    }
+
+                    Debug.WriteLine("DONE");
+                    break;
+                }
+            }
+        }
+
+        // Horizontal
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="word"></param>
+        private void PlaceHorizontallyRandom(string? word)
+        {
+            for (int boardIdx = 0; boardIdx < _board.Length; boardIdx++)
+            {
+                // 만약 해당 cell이 null이라면 
+                if (_board[boardIdx].Equals('\0'))
+                {
+                    bool nullAsLength = CheckIfNullAsWordLengthHorizontally(boardIdx, word.Length);
+                    if (!nullAsLength) continue;
+
+                    bool nearbyClearEnough = CheckIfNearbyClearEnoughHorizontally(boardIdx, word.Length);
+                    if (!nearbyClearEnough) continue;
+
+                    bool diagonalClearByOne = CheckIfDiagonalClearByOneHorizontally(boardIdx, word.Length);
+                    if (!diagonalClearByOne) continue;
+
+                    for (int wordIdx = 0; wordIdx < word.Length; wordIdx++)
+                    {
+                        _board[boardIdx + wordIdx] = word[wordIdx];
+                    }
+
+                    break;
+                }
+            }
         }
 
         private WordDetail[] SavePlacedWordDetail()
